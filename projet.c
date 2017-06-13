f9218ec60db3c537c3e486a2b16715e6e3a1570d
Gustavo <guga.rodarte@hotmail.com>
2017-06-13 18:15:03 -0300

    Leitura de arquivos ppm em formato ASCII

--- projet.c
+++ projet.c
@@ -39,9 +39,9 @@ typedef struct tagBITMAPINFOHEADER{
 	DWORD biClrImportant;
 }BITMAPINFOHEADER;
 
- struct pixel{
+ typedef struct {
 	int r, g, b;
-}Pixel;
+} pixel;
 
 typedef struct tagBITMAPINFO
 {
@@ -58,16 +58,48 @@ typedef struct tagBITMAPINFO
 	int max;
 	int largura;
 	int altura;
- 	struct pixel **imagem;
-}*Ppm;
+ 	pixel **imagem;
+}Ppm;
 
 
-void ler_ppm(struct ppm *Ppm){
+// void ler_ppm(struct ppm *Ppm){
+//
+// 		int i, j;
+// 		FILE *arquivo;
+//
+// 		char nome_arq[50];
+//     printf("entre com o nome do arquivo\n");
+//     scanf("%s", nome_arq);
+//
+//     if ((arquivo = fopen(nome_arq, "r")) == NULL) {
+//         printf("Erro ao abrir o arquivo %s\n", nome_arq);
+//         exit(1);
+//     }
+//
+//
+// 		fscanf(arquivo, "%s", Ppm->code);
+// 		fscanf(arquivo, "%d", Ppm.largura);
+// 		fscanf(arquivo, "%d", Ppm.altura);
+// 		fscanf(arquivo, "%d", Ppm.max);
+//
+// 	// 	for (i = 0; i < Ppm.altura; i++) {
+// 	// 		 for (j = 0; j < Ppm.largura; j++) {
+// 	// 				 fscanf(arquivo, "%d", &Ppm.imagem[i][j].r);
+// 	// 				 fscanf(arquivo, "%d", &Ppm.imagem[i][j].g);
+// 	// 				 fscanf(arquivo, "%d", &Ppm.imagem[i][j].b);
+// 	// 		 }
+// 	//  }
+//
+// 		fclose(arquivo);
+//
+// }
 
-		int i, j;
-		FILE *arquivo;
 
-		char nome_arq[50];
+void ler_ppm(pixel imagem[MAX][MAX], char *code, int *max, int *coluna, int *linha) {
+    int i, j;
+    FILE *arquivo;
+
+    char nome_arq[50];
     printf("entre com o nome do arquivo\n");
     scanf("%s", nome_arq);
 
@@ -76,29 +108,34 @@ void ler_ppm(struct ppm *Ppm){
         exit(1);
     }
 
+    fscanf(arquivo, "%s", code);
+    fscanf(arquivo, "%d", coluna);
+    fscanf(arquivo, "%d", linha);
+    fscanf(arquivo, "%d", max);
 
-		fscanf(arquivo, "%s", &Ppm->code);
-		fscanf(arquivo, "%d", &Ppm->largura);
-		fscanf(arquivo, "%d", &Ppm->altura);
-		fscanf(arquivo, "%d", &Ppm->max);
+    for (i = 0; i < *linha; i++) {
+        for (j = 0; j < *coluna; j++) {
+            fscanf(arquivo, "%d", &imagem[i][j].r);
+            fscanf(arquivo, "%d", &imagem[i][j].g);
+            fscanf(arquivo, "%d", &imagem[i][j].b);
+        }
+    }
 
-		for (i = 0; i < Ppm->altura; i++) {
-			 for (j = 0; j < Ppm->largura; j++) {
-					 fscanf(arquivo, "%d", &Ppm->imagem[i][j].r);
-					 fscanf(arquivo, "%d", &Ppm->imagem[i][j].g);
-					 fscanf(arquivo, "%d", &Ppm->imagem[i][j].b);
-			 }
-
-		fclose(arquivo);
-	 }
-
-
-
+    fclose(arquivo);
 }
 
 
 int main(int argc, char const *argv[])
 {
-	ler_ppm(Ppm);
+	pixel imagem[MAX][MAX]; //cria uma matriz de pixeis para armazenar a imagem
+	char code[3]; //o código para saber se a imagem é ascii ou binária
+	int max; //o valor máximo de tonalidade de cada pixel
+	int larg, alt; // largura e altura da imagem em pixeis
+	ler_ppm(imagem, code, &max, &larg, &alt);
+	for (int i = 0; i < alt; i++) {
+		for(int j = 0.; j < larg; j++){
+			printf("O pixel é: %d %d %d\n", imagem[i][j].r, imagem[i][j].g, imagem[i][j].b);
+		}
+	};
 	return 0;
 }
